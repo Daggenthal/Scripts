3a773ed5ab4f281e4cbb99cfdd3098b4fcb86afd
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2020-10-19 15:40:48 -0500

    Update Software_Install.sh
    Changed commands to use awk instead in order to make the script more global

--- Software_Install.sh
+++ Software_Install.sh
@@ -5,26 +5,20 @@ printf "Please input the software you'd like to install: "
 read install
 
 {
-codename=$(lsb_release -c)		    # Gets the codename for the distribution, and stores it into a variable called codename.
-fedora=$(cat /etc/fedora-release)
-manjaro=$(lsb_release -d)
+debian=$(awk "NR==4" /etc/os-release)		    # Gets the codename for the distribution, and stores it into a variable called codename.
+fedora=$(awk "NR==3" /etc/os-release)
+arch=$(awk "NR==3" /etc/os-release)
 linuxMint=$(awk "NR==11" /etc/os-release)
 } &> /dev/null				        # This hides the nasty "command not found..." errors for this block of code.
 
 {
-printf "%s\n" "$manjaro"		    # Prints out which codename it grabbed. Entirely useless, only for debugging.
-printf "%s\n" "$codename"
-printf "%s\n" "$fedora"
-} 2>&1 #&> /dev/null 			    # 2>&1 redirects stderr into stdout, therefore hiding the "This command doesn't exist" error.
-
-{
-if [ "$codename" == "Codename:	focal" ] || [ "$codename" == "Codename:	disco" ] || [ "$codename" == "Codename:	eoan" ] || [ "$linuxMint" == "VERSION_CODENAME=ulyana" ] ||  [ "$codename" == "Codename:	tricia" ];
+if [ "$debian" == "ID_LIKE=debian" ];
 then
 	sudo apt-get install -y $install
-elif [ "$fedora" == "Fedora release 32 (Thirty Two)" ] || [ "$fedora" == "Fedora release 31 (Thirty One)" ] || [ "Fedora release 30 (Thirty) " ];
+elif [ "$fedora" == "ID=fedora" ];
 then
 	sudo dnf install -y $install
-elif [ "$manjaro" == "Description:	Manjaro Linux" ];
+elif [ "$arch" == "ID_LIKE=arch" ];
 then
 	sudo pacman -S --noconfirm $install
 fi					                # This just ends the if, else if (elif), and "then" checks.
