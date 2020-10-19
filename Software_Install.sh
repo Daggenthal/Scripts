d51b2262d8f84c038a69266903b26ed220270730
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2020-10-19 14:53:40 -0500

    Update Software_Install.sh
    Re-used a majority of the code from a previous script in order to easily grab the OS being used.

--- Software_Install.sh
+++ Software_Install.sh
@@ -4,4 +4,28 @@ printf "Please input the software you'd like to install: "
 
 read install
 
-sudo apt install -y $install
+{
+codename=$(lsb_release -c)		    # Gets the codename for the distribution, and stores it into a variable called codename.
+fedora=$(cat /etc/fedora-release)
+manjaro=$(lsb_release -d)
+linuxMint=$(awk "NR==11" /etc/os-release)
+} &> /dev/null				        # This hides the nasty "command not found..." errors for this block of code.
+
+{
+printf "%s\n" "$manjaro"		    # Prints out which codename it grabbed. Entirely useless, only for debugging.
+printf "%s\n" "$codename"
+printf "%s\n" "$fedora"
+} 2>&1 #&> /dev/null 			    # 2>&1 redirects stderr into stdout, therefore hiding the "This command doesn't exist" error.
+
+{
+if [ "$codename" == "Codename:	focal" ] || [ "$codename" == "Codename:	disco" ] || [ "$codename" == "Codename:	eoan" ] || [ "$linuxMint" == "VERSION_CODENAME=ulyana" ] ||  [ "$codename" == "Codename:	tricia" ];
+then
+	sudo apt-get install -y $install
+elif [ "$fedora" == "Fedora release 32 (Thirty Two)" ] || [ "$fedora" == "Fedora release 31 (Thirty One)" ] || [ "Fedora release 30 (Thirty) " ];
+then
+	sudo dnf install -y $install
+elif [ "$manjaro" == "Description:	Manjaro Linux" ];
+then
+	sudo pacman -S --noconfirm $install
+fi					                # This just ends the if, else if (elif), and "then" checks.
+}
