a2f4c08c64effe0685d3948bdab1445c937b3b47
Daggenthal <64725094+Daggenthal@users.noreply.github.com>
2020-10-20 15:55:43 -0500

    Update Software_Install.sh
    Added support for openSuse

--- Software_Install.sh
+++ Software_Install.sh
@@ -8,6 +8,7 @@ read install
 debian=$(awk "NR==4" /etc/os-release)		    # Gets the codename for the distribution, and stores it into a variable called codename.
 fedora=$(awk "NR==3" /etc/os-release)
 arch=$(awk "NR==3" /etc/os-release)
+openSuse=$(awk "NR==4" /etc/os-release)
 linuxMint=$(awk "NR==11" /etc/os-release)
 } &> /dev/null				        # This hides the nasty "command not found..." errors for this block of code.
 
@@ -21,5 +22,8 @@ then
 elif [ "$arch" == "ID_LIKE=arch" ];
 then
 	sudo pacman -S --noconfirm $install
+elif [ "$openSuse" == "ID_LIKE=opensuse suse" ];
+then
+	sudo zypper install -y $install
 fi					                # This just ends the if, else if (elif), and "then" checks.
 }
